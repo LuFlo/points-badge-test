0135720f4433623f510afb71501944e9e2e1e79b
Lukas Florea <lukas.florea@rwth-aachen.de>
2023-09-20 17:38:46 +0200

    We made a mistake! The unittests have to be async as well. That's why we should first test if the unittests can actually fail, for example by changing a bound value temporarily.
    The error appeared after implementing additional tests for the new
    badges and realizing that everything is green.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,28 +4,52 @@ import { User } from './types/user.interface';
 
 describe('getUsersBadge', () => {
 
-  it(`get Gold`, function () {
-    getUsersBadge(getUserMock(100)).then(res => expect(res).toEqual(Icon.BADGE_GOLD));
-    getUsersBadge(getUserMock(50)).then(res => expect(res).toEqual(Icon.BADGE_GOLD));
-    getUsersBadge(getUserMock(1000000)).then(res => expect(res).toEqual(Icon.BADGE_GOLD));
+  it(`get God Like`, async () => {
+    await getUsersBadge(getUserMock(2001)).then(res => expect(res).toEqual(Icon.BADGE_GOD_LIKE));
+    await getUsersBadge(getUserMock(3000)).then(res => expect(res).toEqual(Icon.BADGE_GOD_LIKE));
+    await getUsersBadge(getUserMock(100000)).then(res => expect(res).toEqual(Icon.BADGE_GOD_LIKE));
   });
 
-  it(`get Silver`, function () {
-    getUsersBadge(getUserMock(25)).then(res => expect(res).toEqual(Icon.BADGE_SILVER));
-    getUsersBadge(getUserMock(49)).then(res => expect(res).toEqual(Icon.BADGE_SILVER));
-    getUsersBadge(getUserMock(30)).then(res => expect(res).toEqual(Icon.BADGE_SILVER));
+  it(`get Platinum`, async () => {
+    await getUsersBadge(getUserMock(101)).then(res => expect(res).toEqual(Icon.BADGE_PLATINUM));
+    await getUsersBadge(getUserMock(150)).then(res => expect(res).toEqual(Icon.BADGE_PLATINUM));
+    await getUsersBadge(getUserMock(2000)).then(res => expect(res).toEqual(Icon.BADGE_PLATINUM));
   });
 
-  it(`get Bronze`, function () {
-    getUsersBadge(getUserMock(5)).then(res => expect(res).toEqual(Icon.BADGE_BRONZE));
-    getUsersBadge(getUserMock(24)).then(res => expect(res).toEqual(Icon.BADGE_BRONZE));
-    getUsersBadge(getUserMock(10)).then(res => expect(res).toEqual(Icon.BADGE_BRONZE));
+  it(`get Gold`, async () => {
+    await getUsersBadge(getUserMock(100)).then(res => expect(res).toEqual(Icon.BADGE_GOLD));
+    await getUsersBadge(getUserMock(50)).then(res => expect(res).toEqual(Icon.BADGE_GOLD));
+    await getUsersBadge(getUserMock(75)).then(res => expect(res).toEqual(Icon.BADGE_GOLD));
   });
 
-  it(`get no Icon`, function () {
-    getUsersBadge(getUserMock(4)).then(res => expect(res).toEqual(null));
-    getUsersBadge(getUserMock(-100)).then(res => expect(res).toEqual(null));
-    getUsersBadge(getUserMock(0)).then(res => expect(res).toEqual(null));
+  it(`get Silver`, async () => {
+    await getUsersBadge(getUserMock(25)).then(res => expect(res).toEqual(Icon.BADGE_SILVER));
+    await getUsersBadge(getUserMock(49)).then(res => expect(res).toEqual(Icon.BADGE_SILVER));
+    await getUsersBadge(getUserMock(30)).then(res => expect(res).toEqual(Icon.BADGE_SILVER));
+  });
+
+  it(`get Bronze`, async () => {
+    await getUsersBadge(getUserMock(5)).then(res => expect(res).toEqual(Icon.BADGE_BRONZE));
+    await getUsersBadge(getUserMock(24)).then(res => expect(res).toEqual(Icon.BADGE_BRONZE));
+    await getUsersBadge(getUserMock(10)).then(res => expect(res).toEqual(Icon.BADGE_BRONZE));
+  });
+
+  it(`get Starter`, async () => {
+    await getUsersBadge(getUserMock(2)).then(res => expect(res).toEqual(Icon.BADGE_STARTER));
+    await getUsersBadge(getUserMock(3)).then(res => expect(res).toEqual(Icon.BADGE_STARTER));
+    await getUsersBadge(getUserMock(4)).then(res => expect(res).toEqual(Icon.BADGE_STARTER));
+  });
+
+  it(`get Bad Ass`, async () => {
+    await getUsersBadge(getUserMock(-1)).then(res => expect(res).toEqual(Icon.BADGE_BAD_ASS));
+    await getUsersBadge(getUserMock(-100)).then(res => expect(res).toEqual(Icon.BADGE_BAD_ASS));
+    await getUsersBadge(getUserMock(-10000000)).then(res => expect(res).toEqual(Icon.BADGE_BAD_ASS));
+  });
+
+  it(`get no Icon`, async () => {
+    await getUsersBadge(getUserMock(4)).then(res => expect(res).toEqual(null));
+    await getUsersBadge(getUserMock(-100)).then(res => expect(res).toEqual(null));
+    await getUsersBadge(getUserMock(0)).then(res => expect(res).toEqual(null));
   });
 
 });
