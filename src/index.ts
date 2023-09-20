495b1cb425ef34c530ad325f4379bccc461371ef
Lukas Florea <lukas.florea@rwth-aachen.de>
2023-09-20 16:45:47 +0200

    Solution 1: The flexible approach. The Badge tests are stored in an array and could be loaded from somewhere else. That way the function getUsersBadge is only responsible for iterating over the badgeTypes.
    Disadvantage: We still have seperate comparison tests for each Badge. This
    is quite error prone, since we have to make sure, that the limits for the
    intervals line up. One could for example set the upper interval for bronze
    to 24 instead of 25. People with exactly 24 solution would not get any
    badge. This error could be hard to detect, if unit tests do not cover
    edge cases sufficiently.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import { User } from './types/user.interface';
 import { Icon } from './types/icon.enum';
 
+type BadgeTest = (solutionCount: number) => boolean;
+
+let badgeTypes: {icon: Icon, testFunction: BadgeTest}[] = [
+  {icon: Icon.BADGE_BRONZE,  testFunction: solutionCount => solutionCount >= 5 && solutionCount < 25},
+  {icon: Icon.BADGE_SILVER,  testFunction: solutionCount => solutionCount >= 25 && solutionCount < 50},
+  {icon: Icon.BADGE_GOLD,  testFunction: solutionCount => solutionCount >= 50}
+];
+
 export const getUsersBadge = ( user: User ): Icon | null => {
-  let badge = null;
-  switch ( true ) {
-    case ( user.solutionCount >= 5 && user.solutionCount < 25 ):
-      badge = Icon.BADGE_BRONZE;
-      break;
-    case ( user.solutionCount >= 25 && user.solutionCount < 50 ):
-      badge = Icon.BADGE_SILVER;
-      break;
-    case ( user.solutionCount >= 50 ):
-      badge = Icon.BADGE_GOLD;
-      break;
-  }
+  let badge: Icon | null = null;
+  for (var badgeType of badgeTypes) {
+    if (badgeType.testFunction(user.solutionCount)) {
+      return badgeType.icon;   
+    }
+  };
   return badge;
 };
